00360e64aeff6294d9b3d9ed511c0a53417410d8
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2020-04-15 15:15:27 -0400

    Temp

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,6 +6,7 @@ use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Wa72\HtmlPageDom\HtmlPageCrawler;
 
 class MainController extends AbstractController
@@ -105,55 +106,64 @@ class MainController extends AbstractController
             throw new NotFoundHttpException;
         }
 
-        $parser->code_class_prefix = 'language-';
+        $pathInfo = pathInfo($filename);
+        if ($pathInfo['extension'] == 'md') {
+            $parser->code_class_prefix = 'language-';
+            $parser->url_filter_func = function ($url) {
+                dump($url);
+                return $url;
+            };
 
-        $content = $parser->transformMarkdown(file_get_contents($filename));
+            $content = $parser->transformMarkdown(file_get_contents($filename));
 
-        /**
-         * The content should be wrapped because the crawler has bug when deleting root nodes.
-         * @see https://github.com/wasinger/htmlpagedom/issues/25#issuecomment-507515271
-         */
-        $content = HtmlPageCrawler::create('<div class="docs-content">'.$content.'</div>');
+            /**
+             * The content should be wrapped because the crawler has bug when deleting root nodes.
+             * @see https://github.com/wasinger/htmlpagedom/issues/25#issuecomment-507515271
+             */
+            $content = HtmlPageCrawler::create('<div class="docs-content">'.$content.'</div>');
 
-        $contentTitleElement = $content->filter('h1');
-        $contentTitle = $contentTitleElement->getInnerHtml();
+            $contentTitleElement = $content->filter('h1');
+            $contentTitle = $contentTitleElement->getInnerHtml();
 
-        $contentTitleElement->remove();
+            $contentTitleElement->remove();
 
-        $content->filter('pre > code:not([class])')->addClass('language-markup');
+            $content->filter('pre > code:not([class])')->addClass('language-markup');
 
-        $menu = [];
-        $content->filter('h2')->each(function ($h2, $i) use (&$menu) {
-            $text = $h2->getInnerHtml();
-            $n = $i + 1;
+            $menu = [];
+            $content->filter('h2')->each(function ($h2, $i) use (&$menu) {
+                $text = $h2->getInnerHtml();
+                $n = $i + 1;
 
-            $linkText = "{$n}. {$text}";
-            $h2->setInnerHtml($linkText);
+                $linkText = "{$n}. {$text}";
+                $h2->setInnerHtml($linkText);
 
-            $id = str_replace('.', '', 'l-'.$linkText);
-            $id = str_replace(' ', '-', $id);
-            $id = str_replace('á', 'a', $id);
-            $id = str_replace('é', 'e', $id);
-            $id = str_replace('í', 'i', $id);
-            $id = str_replace('ó', 'o', $id);
-            $id = str_replace('ú', 'u', $id);
-            $id = str_replace('Í', 'I', $id);
-            $id = strtolower($id);
-            $h2->setAttribute('id', $id);
+                $id = str_replace('.', '', 'l-'.$linkText);
+                $id = str_replace(' ', '-', $id);
+                $id = str_replace('á', 'a', $id);
+                $id = str_replace('é', 'e', $id);
+                $id = str_replace('í', 'i', $id);
+                $id = str_replace('ó', 'o', $id);
+                $id = str_replace('ú', 'u', $id);
+                $id = str_replace('Í', 'I', $id);
+                $id = strtolower($id);
+                $h2->setAttribute('id', $id);
 
-            $menu[] = ['id' => $id, 'text' => $linkText];
-        });
+                $menu[] = ['id' => $id, 'text' => $linkText];
+            });
 
-        $template = empty($menu) ?
-            'devAid/page-docs-without-menu.html.twig' :
-            'devAid/page-docs.html.twig'
-        ;
+            $template = empty($menu) ?
+                'devAid/page-docs-without-menu.html.twig' :
+                'devAid/page-docs.html.twig'
+            ;
 
-        return $this->render($template, [
-            'navigation' => [],
-            'content' => $content,
-            'contentTitle' => $contentTitle,
-            'menu' => $menu,
-        ]);
+            return $this->render($template, [
+                'navigation' => [],
+                'content' => $content,
+                'contentTitle' => $contentTitle,
+                'menu' => $menu,
+            ]);
+        } else {
+            return new BinaryFileResponse($filename);
+        }
     }
 }
