95bd2d5b9def1c0f7d5f0915d0b621f707c28e75
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2020-06-20 11:47:37 -0400

    Temp

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,6 +12,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use App\Entity\BlogPost;
 use App\Entity\Page;
 use App\Repository\BlogPostRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class MainController extends AbstractController
 {
@@ -36,6 +37,12 @@ class MainController extends AbstractController
                     'a_class'  => 'nav-link scrollto',
                     'href'     => '#projects',
                     'text'     => 'Proyectos',
+                ],
+                [
+                    'li_class' => 'nav-item',
+                    'a_class'  => 'nav-link scrollto',
+                    'href'     => '#blog',
+                    'text'     => 'Blog',
                 ],
                 [
                     'li_class' => 'nav-item',
@@ -91,29 +98,7 @@ class MainController extends AbstractController
             $content->filter('pre > code:not([class])')->addClass('language-markup');
             $content->filter('img:not([class])')->addClass('img-fluid');
 
-            $menu = [];
-            $content->filter('h2')->each(function ($h2, $i) use (&$menu) {
-                $text = $h2->getInnerHtml();
-                $n = $i + 1;
-
-                $linkText = "{$n}. {$text}";
-                $h2->setInnerHtml($linkText);
-
-                $id = str_replace('.', '', 'l-'.$linkText);
-                $id = str_replace(' ', '-', $id);
-                $id = str_replace('á', 'a', $id);
-                $id = str_replace('é', 'e', $id);
-                $id = str_replace('í', 'i', $id);
-                $id = str_replace('ó', 'o', $id);
-                $id = str_replace('ú', 'u', $id);
-                $id = str_replace('Í', 'I', $id);
-                $id = strtolower($id);
-                $h2->setAttribute('id', $id);
-
-                $menu[] = ['id' => $id, 'text' => $linkText];
-            });
-
-            $template = 'devAid/page-docs.html.twig';
+            $menu = $this->getMenu($content);
 
             return $this->render('devAid/page-docs.html.twig', [
                 'content' => $content,
@@ -125,6 +110,16 @@ class MainController extends AbstractController
         } else {
             return new BinaryFileResponse($filename);
         }
+    }
+
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function blog()
+    {
+        return new RedirectResponse(
+            $this->generateUrl('index').'#blog'
+        );
     }
 
     /**
@@ -155,8 +150,27 @@ class MainController extends AbstractController
         $content->filter('img:not([class])')->addClass('img-fluid');
 
         $title = $entity->getTitle();
+        $menu = $this->getMenu($content);
 
+        $ogDescription = $entity instanceof BlogPost ?
+            substr($entity->getAbstract(), 0, 195) :
+            ''
+        ;
+
+        return $this->render('devAid/page-docs.html.twig', [
+            'content' => $content,
+            'contentTitle' => $title,
+            'pageTitle' => $title,
+            'meta_description' => "{$title} | ThenLabs",
+            'ogDescription' => $ogDescription,
+            'menu' => $menu,
+        ]);
+    }
+
+    public function getMenu($content)
+    {
         $menu = [];
+
         $content->filter('h2')->each(function ($h2, $i) use (&$menu) {
             $text = $h2->getInnerHtml();
             $n = $i + 1;
@@ -178,12 +192,6 @@ class MainController extends AbstractController
             $menu[] = ['id' => $id, 'text' => $linkText];
         });
 
-        return $this->render('devAid/page-docs.html.twig', [
-            'content' => $content,
-            'contentTitle' => $title,
-            'pageTitle' => $title,
-            'meta_description' => "{$title} | ThenLabs",
-            'menu' => $menu,
-        ]);
+        return $menu;
     }
 }
