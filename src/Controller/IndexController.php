4cdaf45b8a77d52b7fd6833aa96e1f52a93ba9c3
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2020-07-22 16:15:13 -0400

    Temp

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use App\Entity\Invoice;
 
@@ -44,5 +46,45 @@ class IndexController extends AbstractController
         $entityManager->flush();
 
         return new Response('*ok*');
+    }
+
+    /**
+     * @Route("/invoiceAddress", name="invoice_address")
+     */
+    public function invoiceAddress()
+    {
+        $xpub = $_ENV['BLOCKCHAIN_XPUB'];
+        $apiKey = $_ENV['BLOCKCHAIN_API_KEY'];
+
+        $invoiceId = time();
+
+        $routeParameters = [
+            'invoice_id' => $invoiceId,
+            'secret' => $_ENV['BLOCKCHAIN_SECRET'],
+        ];
+
+        $callbackUrl = $this->generateUrl(
+            'invoice',
+            $routeParameters,
+            UrlGeneratorInterface::ABSOLUTE_URL
+        );
+
+        $root_url = 'https://api.blockchain.info/v2/receive';
+        $parameters = http_build_query([
+            'xpub' => $xpub,
+            'key' => $apiKey,
+            'callback' => $callbackUrl,
+        ]);
+
+        $url = $root_url.'?'.$parameters;
+
+        $response = file_get_contents($url);
+        $object = json_decode($response);
+
+        if (is_object($object) && isset($object->address)) {
+            return new JsonResponse(['address' => $object->address]);
+        } else {
+            throw new Exception;
+        }
     }
 }
